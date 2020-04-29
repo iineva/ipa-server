84a1714af2858037762340ce80199188f33c3d4c
Steven <s@ineva.cn>
2020-04-29 19:43:33 +0800

    Fix Chinese path parse bug.

--- libs/ipa-manager.js
+++ libs/ipa-manager.js
@@ -70,8 +70,8 @@ const add = async (file) => {
   let plistFile, iconFiles = []
 
   // unzip files
-  const newIconRegular = /Payload\/\w*\.app\/AppIcon-?(\d+(\.\d+)?)x(\d+(\.\d+)?)(@\dx)?.*\.png$/
-  const oldIconRegular = /Payload\/\w*\.app\/Icon-?(\d+(\.\d+)?)?.png$/
+  const newIconRegular = /Payload\/.*\.app\/AppIcon-?(\d+(\.\d+)?)x(\d+(\.\d+)?)(@\dx)?.*\.png$/
+  const oldIconRegular = /Payload\/.*\.app\/Icon-?(\d+(\.\d+)?)?.png$/
   await fs.remove(tmpDir)
   await decompress({
     file: file,
